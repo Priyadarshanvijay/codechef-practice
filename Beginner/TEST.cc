d47794b980563479ba9ae074e38f5c8258572fc5
Priyadarshan Vijay <priyadarshanvijay@gmail.com>
2020-06-03 15:03:33 +0530

    Solved Life, the Universe, and Everything

---
+++ Beginner/TEST.cc
@@ -0,0 +1,13 @@
+// Life, the Universe, and Everything
+// priyadarshanvijay@gmail.com
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int x = 0;
+  while(cin >> x) {
+    if( x == 42) break;
+    cout << x << '\n';
+  }
+  return 0;
+}
